b3f70cb0bb86bc1160a85b218bf21ed9f41be270
Jacob Scanlon <jc974930@dal.ca>
2018-06-13 22:36:48 -0300

    Manifest Activities fix
    onBackPressed() was being handled the wrong way. System default functions properly when multiple activities are defined properly. Only the first activity should be marked as launcher, as multiple launchers create multiple app icons that open each activity respectively from the system launcher. Other activities should be marked as alternative so the back button works as expected, and multiple app icons are not created

--- app/src/main/java/com/example/scott/dalmapproject/ClassInfoActivity.java
+++ app/src/main/java/com/example/scott/dalmapproject/ClassInfoActivity.java
@@ -1,6 +1,5 @@
 package com.example.scott.dalmapproject;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -13,12 +12,4 @@ public class ClassInfoActivity extends AppCompatActivity {
 
     }
 
-    public void onBackPressed(){
-        super.onBackPressed();
-
-        Intent intent = new Intent(this, ClassListActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_TASK_ON_HOME);
-
-        startActivity(intent);
-    }
 }
