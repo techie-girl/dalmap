f56e338c5269e60c206b8c7b82e882a5554f7100
Jacob Scanlon <jc974930@dal.ca>
2018-06-13 23:30:15 -0300

    Popup Window Creation
    Created a popup window to display the class info instead of launching another activity. Currently just goes away when pressed anywhere, will be changed to have a button to close and any other buttons for further functionality. TextViews in the popup window are temporary and will be refactored to cleanly display the class information.
    
    Created an anim file to hold custom animations. Added 2 animations that just fade the popup window in and out respectively. Modified styles to point the popup window at the animations
    
    Removed the intent from ClassListActivity because it is no longer used. Now a layout is drawn on top of the class list instead of launching a new activity for the layout.

--- app/src/main/java/com/example/scott/dalmapproject/ClassListActivity.java
+++ app/src/main/java/com/example/scott/dalmapproject/ClassListActivity.java
@@ -3,10 +3,16 @@ package com.example.scott.dalmapproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -23,13 +29,11 @@ public class ClassListActivity extends AppCompatActivity {
     protected void onStart(){
         super.onStart();
 
-        ListView listView = this.findViewById(R.id.Class_List_List_View);
+        final ListView listView = this.findViewById(R.id.Class_List_List_View);
 
         ArrayList<String> List = new ArrayList<>();
         List.add("Item 1");
         List.add("Item 2");
-
-        final Intent intent = new Intent(this, ClassInfoActivity.class);
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
                 this,
@@ -43,7 +47,34 @@ public class ClassListActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position,
                                     long id) {
 
-                startActivity(intent);
+                LayoutInflater inflater = (LayoutInflater)
+                        getSystemService(LAYOUT_INFLATER_SERVICE);
+                View popupView = inflater.inflate(R.layout.class_info_popup, null);
+
+                int width = LinearLayout.LayoutParams.MATCH_PARENT;
+                int height = LinearLayout.LayoutParams.MATCH_PARENT;
+                boolean focusable = true;
+                final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+                String selectedItem = listView.getItemAtPosition(position).toString();
+
+                ((TextView) popupWindow.getContentView().findViewById(R.id.ClassCode)).setText(selectedItem);
+                ((TextView) popupWindow.getContentView().findViewById(R.id.ClassTitle)).setText(selectedItem);
+                ((TextView) popupWindow.getContentView().findViewById(R.id.ClassDuration)).setText(selectedItem);
+                ((TextView) popupWindow.getContentView().findViewById(R.id.ClassLocation)).setText(selectedItem);
+
+                popupWindow.setAnimationStyle(R.style.Animation);
+
+                popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+                // dismiss the popup window when touched
+                popupView.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        popupWindow.dismiss();
+                        return true;
+                    }
+                });
             }
         });
     }
