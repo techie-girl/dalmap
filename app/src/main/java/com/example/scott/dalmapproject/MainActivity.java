d792b432c32a18756088fe619ee966972995cf0b
Jacob Scanlon <jc974930@dal.ca>
2018-06-18 03:58:10 -0300

    Remade NotificationService as a Broadcast Receiver.
    Deleted NotificationService because it is not compatible with AlarmManager. Copied important code for building notification to MainActivity for a temporary home, will later be refactored into its own service class.
    
    Added AlarmManager support to MainActivity so that it can be set up to create notifications at a specific time in the future.
    
    Created NotificationPublisher to act as the Broadcast Receiver for the AlarmManager.
    
    Added classes.xml to hold some class information temporarily until firebase support is set up.
    
    Used the information in classes.xml to begin sending notifications for the users next class. Needs to be improved so that it can check all of the users classes for the soonest time instead of just the first class in their list. Also there is an issue with running this process a few minutes before the hour ends, causing an extra hour to be added before the notification is sent. Also applies to minutes and sets of 60 seconds. Will be corrected soon, left as TODOs right now.
    
    TextView in activity_main is overwrote with some testing information currently, But doesn't need to exist as all as this is will all be refactored into service and broadcast receiver work in the end.

--- app/src/main/java/com/example/scott/dalmapproject/MainActivity.java
+++ app/src/main/java/com/example/scott/dalmapproject/MainActivity.java
@@ -1,11 +1,21 @@
 package com.example.scott.dalmapproject;
 
+import android.app.AlarmManager;
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,10 +48,58 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onStop(){
-        super.onStop();
+    protected void onStart(){
+        super.onStart();
 
-        //Starts the NotificationService
-        startService(new Intent(this, NotificationService.class));
+        String[] csci1000 = getResources().getStringArray(R.array.CSCI1000);
+        String[] csci1001 = getResources().getStringArray(R.array.CSCI1001);
+        String[] csci1002 = getResources().getStringArray(R.array.CSCI1002);
+        String[] csci1003 = getResources().getStringArray(R.array.CSCI1003);
+
+        Date date = Calendar.getInstance().getTime();
+        TextView textView = findViewById(R.id.this_text);
+
+        String currentTime = date.toString();
+
+        String currentHour = currentTime.substring(11,13);
+        String currentMinute = currentTime.substring(14,16);
+        String currentSecond = currentTime.substring(17,19);
+
+        //TODO make this happen for the soonest class after current time, not just the first class in the users list
+        //TODO For now, to test, just set the time in classes.xml csci1000[1] to be a minute or two ahead.
+        int goalHour = Integer.valueOf(csci1000[1].substring(0,2));
+        int goalMinute = Integer.valueOf(csci1000[1].substring(3, 5));
+
+        //TODO correct this so that it sends when i want it, not potentially 01:02:00 later in extreme but possible case
+        Calendar c = Calendar.getInstance();
+        c.add(Calendar.DAY_OF_MONTH, 0);
+        c.add(Calendar.HOUR_OF_DAY, goalHour-Integer.valueOf(currentHour));
+        c.add(Calendar.MINUTE, goalMinute-Integer.valueOf(currentMinute));
+        c.add(Calendar.SECOND, 60-Integer.valueOf(currentSecond));
+        c.add(Calendar.MILLISECOND, 0);
+
+        textView.setText(currentHour+currentMinute+currentSecond+":"+goalHour+goalMinute+":"+c.getTimeInMillis()%System.currentTimeMillis());
+
+        scheduleNotification(getNotification(String.valueOf(c.getTimeInMillis()%System.currentTimeMillis())), c.getTimeInMillis()%System.currentTimeMillis());
+    }
+
+    private void scheduleNotification(Notification notification, long delay) {
+
+        Intent notificationIntent = new Intent(this, NotificationPublisher.class);
+        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION_ID, 1);
+        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION, notification);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        long futureInMillis = SystemClock.elapsedRealtime() + delay;
+        AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureInMillis, pendingIntent);
+    }
+
+    private Notification getNotification(String content) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Class Reminder")
+            .setContentText("Class starts in " + Long.parseLong(content)/60000 + " minutes")
+            .setSmallIcon(R.drawable.ic_launcher_background);
+        return builder.build();
     }
 }
