99b315f4affb7c36010331c06677a57bb9c96130
Jacob Scanlon <jc974930@dal.ca>
2018-07-04 12:53:18 -0300

    Added new image view activity
    Added an image view for the killam floor plan images. It scrolls through the images on click.

--- app/src/main/java/com/example/scott/dalmapproject/KillamInfoActivity.java
+++ app/src/main/java/com/example/scott/dalmapproject/KillamInfoActivity.java
@@ -1,7 +1,11 @@
 package com.example.scott.dalmapproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class KillamInfoActivity extends AppCompatActivity {
@@ -13,8 +17,20 @@ public class KillamInfoActivity extends AppCompatActivity {
 
         TextView Services = (TextView) findViewById(R.id.Services);
         TextView Building = (TextView) findViewById(R.id.building);
+        ImageButton kilFloorPlanButton = (ImageButton) findViewById(R.id.kil_floor_plan_button);
+
         //Set service info text
         Services.setText("Services: Bistro\nHours: 11-5 Mon-Fri\nLocation: Killam Library South Learning Commons");
         Building.setText("Hours:7:30am-9pm\n");
+
+        kilFloorPlanButton.setImageResource(R.drawable.kil_1);
+
+        kilFloorPlanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent LaunchKillamFloorPlan = new Intent(v.getContext(), KillamFloorPlanActivity.class);
+                startActivity(LaunchKillamFloorPlan);
+            }
+        });
     }
 }
