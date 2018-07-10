3da2aabf43c96043b3eefcb31ab110efa2926723
JScanl <jc974930@dal.ca>
2018-07-10 20:36:40 -0300

    Added zoom support to floor plans

--- app/src/main/java/com/example/scott/dalmapproject/KillamFloorPlanActivity.java
+++ app/src/main/java/com/example/scott/dalmapproject/KillamFloorPlanActivity.java
@@ -1,11 +1,38 @@
 package com.example.scott.dalmapproject;
 
+import android.graphics.Matrix;
+import android.graphics.PointF;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.FloatMath;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
-public class KillamFloorPlanActivity extends AppCompatActivity{
+public class KillamFloorPlanActivity extends AppCompatActivity implements View.OnTouchListener{
+
+    private static final String TAG = "Touch";
+    @SuppressWarnings("unused")
+    private static final float MIN_ZOOM = 1f,MAX_ZOOM = 1f;
+
+    // These matrices will be used to scale points of the image
+    Matrix matrix = new Matrix();
+    Matrix savedMatrix = new Matrix();
+
+    // The 3 states (events) which the user is trying to perform
+    static final int NONE = 0;
+    static final int DRAG = 1;
+    static final int ZOOM = 2;
+    int mode = NONE;
+
+    // these PointF objects are used to record the point(s) the user is touching
+    PointF start = new PointF();
+    PointF mid = new PointF();
+    float oldDist = 1f;
+
+    int currentImage = 1;
 
     protected void onCreate(Bundle savedBundleState){
         super.onCreate(savedBundleState);
@@ -13,23 +40,113 @@ public class KillamFloorPlanActivity extends AppCompatActivity{
 
         final ImageView killamFloorPlanImage = (ImageView) findViewById(R.id.killam_floor_plan_image);
 
-        final int[] levels = {R.drawable.kil_basement, R.drawable.kil_1, R.drawable.kil_2,
-                R.drawable.kil_3, R.drawable.kil_4, R.drawable.kil_5};
+        Button nextFloorButton = (Button) findViewById(R.id.killam_floor_plan_next_button);
+        Button previousFloorButton = (Button) findViewById(R.id.killam_floor_plan_back_button);
 
-        killamFloorPlanImage.setImageResource(R.drawable.kil_1);
+        killamFloorPlanImage.setOnTouchListener(this);
 
-        killamFloorPlanImage.setOnClickListener(new View.OnClickListener() {
+        final int[] levels = {R.mipmap.kil_basement, R.mipmap.kil_1, R.mipmap.kil_2,
+                R.mipmap.kil_3, R.mipmap.kil_4, R.mipmap.kil_5};
 
-            int imageTracker = 1;
+        killamFloorPlanImage.setImageResource(R.mipmap.kil_1);
 
+        nextFloorButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                killamFloorPlanImage.setImageResource(levels[++imageTracker]);
-
-                if(imageTracker == 5){
-                    imageTracker = -1;
+                if(currentImage == 5){
+                    currentImage = -1;
                 }
+
+                killamFloorPlanImage.setImageResource(levels[++currentImage]);
+            }
+        });
+
+        previousFloorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(currentImage == 0){
+                    currentImage = 6;
+                }
+                killamFloorPlanImage.setImageResource(levels[--currentImage]);
             }
         });
     }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        ImageView view = (ImageView) v;
+        view.setScaleType(ImageView.ScaleType.MATRIX);
+        float scale;
+
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:   // first finger down only
+                savedMatrix.set(matrix);
+                start.set(event.getX(), event.getY());
+                mode = DRAG;
+                break;
+
+            case MotionEvent.ACTION_UP: // first finger lifted
+            case MotionEvent.ACTION_POINTER_UP: // second finger lifted
+                mode = NONE;
+                break;
+
+            case MotionEvent.ACTION_POINTER_DOWN: // first and second finger down
+                oldDist = spacing(event);
+                if (oldDist > 5f) {
+                    savedMatrix.set(matrix);
+                    midPoint(mid, event);
+                    mode = ZOOM;
+                }
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+
+                if (mode == DRAG){
+                    matrix.set(savedMatrix);
+                    matrix.postTranslate(event.getX() - start.x, event.getY() - start.y); // create the transformation in the matrix  of points
+                }
+                else if (mode == ZOOM) {
+                    // pinch zooming
+                    float newDist = spacing(event);
+                    if (newDist > 5f) {
+                        matrix.set(savedMatrix);
+                        scale = newDist / oldDist; // setting the scaling of the
+                        // matrix...if scale > 1 means
+                        // zoom in...if scale < 1 means
+                        // zoom out
+                        matrix.postScale(scale, scale, mid.x, mid.y);
+                    }
+                }
+                break;
+        }
+        view.setImageMatrix(matrix); // display the transformation on screen
+        return true; // indicate event was handled
+    }
+
+    /*
+     * --------------------------------------------------------------------------
+     * Method: spacing Parameters: MotionEvent Returns: float Description:
+     * checks the spacing between the two fingers on touch
+     * ----------------------------------------------------
+     */
+
+    private float spacing(MotionEvent event) {
+        float x = event.getX(0) - event.getX(1);
+        float y = event.getY(0) - event.getY(1);
+        return (float) Math.sqrt(x * x + y * y);
+    }
+
+    /*
+     * --------------------------------------------------------------------------
+     * Method: midPoint Parameters: PointF object, MotionEvent Returns: void
+     * Description: calculates the midpoint between the two fingers
+     * ------------------------------------------------------------
+     */
+
+    private void midPoint(PointF point, MotionEvent event) {
+        float x = event.getX(0) + event.getX(1);
+        float y = event.getY(0) + event.getY(1);
+        point.set(x / 2, y / 2);
+    }
+
 }
