b9bc9cc381a302c95d1901ea73c35eb1eab7b996
Jacob Scanlon <jc974930@dal.ca>
2018-07-26 03:22:02 -0300

    Mapping & Refactoring
    Implemented simple mapping.
    Set default location to the killam library.
    Removed labels from the map using style strings.
    Added a few points for our buildings of interest.
    Changed minimum version to android 23 for permissions.
    Implemented user location.
    Enabled user location button.
    Added buttons to go between interior and exterior maps.
    While adding buttons to killam floor plan, I refactored it.
    While adding button to killam info, I refactored it.
    Changed image buttons to buttons in killam info. (reduced lag and start of UI rework)

--- app/src/main/java/com/example/scott/dalmapproject/KillamFloorPlanActivity.java
+++ app/src/main/java/com/example/scott/dalmapproject/KillamFloorPlanActivity.java
@@ -12,6 +12,7 @@
  */
 package com.example.scott.dalmapproject;
 
+import android.content.Intent;
 import android.graphics.Matrix;
 import android.graphics.PointF;
 import android.os.Bundle;
@@ -48,121 +49,47 @@ public class KillamFloorPlanActivity extends AppCompatActivity implements View.O
 
         final ImageView killamFloorPlanImage = (ImageView) findViewById(R.id.killam_floor_plan_image);
 
-        Button nextFloorButton = (Button) findViewById(R.id.killam_floor_plan_next_button);
-        Button previousFloorButton = (Button) findViewById(R.id.killam_floor_plan_back_button);
-        Button mappingButton = (Button) findViewById(R.id.killam_floor_plan_mapping_button);
-        final TextView travelTimeText = (TextView) findViewById(R.id.killam_floor_plan_traveltime_text);
-
         killamFloorPlanImage.setOnTouchListener(this);
-
-
-        final int[] levels = {R.mipmap.kil_basement, R.mipmap.kil_1, R.mipmap.kil_2,
-                R.mipmap.kil_3, R.mipmap.kil_4, R.mipmap.kil_5};
 
         killamFloorPlanImage.setImageResource(R.mipmap.kil_1);
 
-        mappingButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                maping = true;
-                travelTimeText.setText("Click your current \nlocation and destination at the same time");
-            }
-        });
+        //button functionality
+        mappingButtonFunction();
+        nextFloorButtonFunction();
+        exteriorMapButtonFunction();
+        previousFloorButtonFunction();
 
-        nextFloorButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(currentImage == 5){
-                    currentImage = -1;
-                }
-
-                killamFloorPlanImage.setImageResource(levels[++currentImage]);
-            }
-        });
-
-        previousFloorButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(currentImage == 0){
-                    currentImage = 6;
-                }
-                killamFloorPlanImage.setImageResource(levels[--currentImage]);
-            }
-        });
     }
 
+    /**
+     * Response to touch events in the image view
+     *
+     * @param v - Image view
+     * @param event - Touch event
+     * @return - acknowledgment of the event
+     */
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-        ImageView view = (ImageView) v;
-        view.setScaleType(ImageView.ScaleType.MATRIX);
-        float scale, x, y;
 
         if(!maping) {
-            switch (event.getAction() & MotionEvent.ACTION_MASK) {
-                case MotionEvent.ACTION_DOWN:   // first finger down only
-                    savedMatrix.set(matrix);
-                    start.set(event.getX(), event.getY());
-                    mode = DRAG;
-                    break;
-
-                case MotionEvent.ACTION_UP: // first finger lifted
-                case MotionEvent.ACTION_POINTER_UP: // second finger lifted
-                    mode = NONE;
-                    break;
-
-                case MotionEvent.ACTION_POINTER_DOWN: // first and second finger down
-                    x = event.getX(0) - event.getX(1);
-                    y = event.getY(0) - event.getY(1);
-                    oldDist = (float) Math.sqrt(x * x + y * y);
-                    if (oldDist > 5f) {
-                        savedMatrix.set(matrix);
-
-                        x = event.getX(0) + event.getX(1);
-                        y = event.getY(0) + event.getY(1);
-                        mid.set(x / 2, y / 2);
-                        mode = ZOOM;
-                    }
-                    break;
-
-                case MotionEvent.ACTION_MOVE:
-
-                    if (mode == DRAG) {
-                        matrix.set(savedMatrix);
-                        matrix.postTranslate(event.getX() - start.x, event.getY() - start.y); // create the transformation in the matrix  of points
-                    } else if (mode == ZOOM) {
-                        // pinch zooming
-                        x = event.getX(0) - event.getX(1);
-                        y = event.getY(0) - event.getY(1);
-                        float newDist = (float) Math.sqrt(x * x + y * y);
-                        if (newDist > 5f) {
-                            matrix.set(savedMatrix);
-                            scale = newDist / oldDist; // setting the scaling of the
-                            // matrix...if scale > 1 means
-                            // zoom in...if scale < 1 means
-                            // zoom out
-                            matrix.postScale(scale, scale, mid.x, mid.y);
-                        }
-                    }
-                    break;
-            }
-            view.setImageMatrix(matrix); // display the transformation on screen
+            onTouchNotMapping(v, event);
         }
         else{
-            switch (event.getAction() & MotionEvent.ACTION_MASK) {
-                case MotionEvent.ACTION_POINTER_DOWN: // first and second finger down
-                    float sourceX = event.getX(0);
-                    float sourceY = event.getY(0);
-                    float xOfInterest = event.getX(1);
-                    float yOfInterest = event.getY(1);
-                    applyDijkstra(currentImage, (int)xOfInterest, (int)yOfInterest, (int)sourceX, (int)sourceY);
-                    maping = false;
-                    break;
-            }
+            onTouchMapping(event);
         }
 
         return true; // indicate event was handled
     }
 
+    /**
+     * Distance between 2 points functionality
+     *
+     * @param floorOfInterest - Current floor
+     * @param xOfInterest - Destination x
+     * @param yOfInterest - Destination y
+     * @param sourceX - Source x
+     * @param sourceY - Source y
+     */
     public void applyDijkstra(int floorOfInterest, int xOfInterest, int yOfInterest, int sourceX, int sourceY){
 
         final int[] BWs = {R.mipmap.kil_basement_bw, R.mipmap.kil_1_bw, R.mipmap.kil_2_bw,
@@ -188,5 +115,203 @@ public class KillamFloorPlanActivity extends AppCompatActivity implements View.O
         //21 pixels per meter, 1m/s walking speed
         travelTimeText.setText(String.valueOf(distance/21));
 
+    }
+
+    /**
+     * Functionality for mapping button
+     */
+    private void mappingButtonFunction(){
+
+        Button mappingButton = (Button) findViewById(R.id.killam_floor_plan_mapping_button);
+        final TextView travelTimeText = (TextView) findViewById(R.id.killam_floor_plan_traveltime_text);
+
+        mappingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                maping = true;
+                travelTimeText.setText("Click your current \nlocation and destination at the same time");
+            }
+        });
+    }
+
+    /**
+     * Functionality for next floor button
+     */
+    private void nextFloorButtonFunction(){
+
+        final ImageView killamFloorPlanImage = (ImageView) findViewById(R.id.killam_floor_plan_image);
+        final int[] levels = {R.mipmap.kil_basement, R.mipmap.kil_1, R.mipmap.kil_2,
+                R.mipmap.kil_3, R.mipmap.kil_4, R.mipmap.kil_5};
+
+        Button nextFloorButton = (Button) findViewById(R.id.killam_floor_plan_next_button);
+        nextFloorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(currentImage == 5){
+                    currentImage = -1;
+                }
+
+                killamFloorPlanImage.setImageResource(levels[++currentImage]);
+            }
+        });
+    }
+
+    /**
+     * Functionality for exterior map button
+     */
+    private void exteriorMapButtonFunction(){
+
+        Button exteriorMapButton = (Button) findViewById(R.id.killam_floor_plan_to_exterior);
+        exteriorMapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MapActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    /**
+     * Functionality fir previous floor button
+     */
+    private void previousFloorButtonFunction(){
+
+        final ImageView killamFloorPlanImage = (ImageView) findViewById(R.id.killam_floor_plan_image);
+        final int[] levels = {R.mipmap.kil_basement, R.mipmap.kil_1, R.mipmap.kil_2,
+                R.mipmap.kil_3, R.mipmap.kil_4, R.mipmap.kil_5};
+
+        Button previousFloorButton = (Button) findViewById(R.id.killam_floor_plan_back_button);
+        previousFloorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(currentImage == 0){
+                    currentImage = 6;
+                }
+                killamFloorPlanImage.setImageResource(levels[--currentImage]);
+            }
+        });
+    }
+
+    /**
+     * Reaction to touch events while mapping isn't turned on
+     *
+     * @param v - Current image view
+     * @param event - Touch event
+     */
+    private void onTouchNotMapping(View v, MotionEvent event){
+        ImageView view = (ImageView) v;
+        view.setScaleType(ImageView.ScaleType.MATRIX);
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:   // first finger down only
+                onActionDown(event);
+                break;
+
+            case MotionEvent.ACTION_UP: // first finger lifted
+            case MotionEvent.ACTION_POINTER_UP: // second finger lifted
+                onActionUp();
+                break;
+
+            case MotionEvent.ACTION_POINTER_DOWN: // first and second finger down
+                onPointerDown(event);
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                onActionMove(event);
+                break;
+        }
+        view.setImageMatrix(matrix); // display the transformation on screen
+    }
+
+    /**
+     * Reaction to touch events while mapping is turned on
+     *
+     * @param event - Touch event
+     */
+    private void onTouchMapping(MotionEvent event){
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_POINTER_DOWN: // first and second finger down
+                onMappingPointerDown(event);
+                break;
+        }
+    }
+
+    /**
+     * Reaction to a down action touch event
+     *
+     * @param event - Touch event
+     */
+    private void onActionDown(MotionEvent event){
+        savedMatrix.set(matrix);
+        start.set(event.getX(), event.getY());
+        mode = DRAG;
+    }
+
+    /**
+     * Reaction to a up action event
+     */
+    private void onActionUp(){
+        mode = NONE;
+    }
+
+    /**
+     * Reaction to a pointer down touch event
+     *
+     * @param event - Touch event
+     */
+    private void onPointerDown(MotionEvent event){
+        float x, y;
+
+        x = event.getX(0) - event.getX(1);
+        y = event.getY(0) - event.getY(1);
+        oldDist = (float) Math.sqrt(x * x + y * y);
+        if (oldDist > 5f) {
+            savedMatrix.set(matrix);
+
+            x = event.getX(0) + event.getX(1);
+            y = event.getY(0) + event.getY(1);
+            mid.set(x / 2, y / 2);
+            mode = ZOOM;
+        }
+    }
+
+    /**
+     * Reaction to a move action event
+     *
+     * @param event - Touch event
+     */
+    private void onActionMove(MotionEvent event){
+        float scale, x, y;
+
+        if (mode == DRAG) {
+            matrix.set(savedMatrix);
+            matrix.postTranslate(event.getX() - start.x, event.getY() - start.y); // create the transformation in the matrix  of points
+        } else if (mode == ZOOM) {
+            // pinch zooming
+            x = event.getX(0) - event.getX(1);
+            y = event.getY(0) - event.getY(1);
+            float newDist = (float) Math.sqrt(x * x + y * y);
+            if (newDist > 5f) {
+                matrix.set(savedMatrix);
+                scale = newDist / oldDist; // setting the scaling of the
+                // matrix...if scale > 1 means
+                // zoom in...if scale < 1 means
+                // zoom out
+                matrix.postScale(scale, scale, mid.x, mid.y);
+            }
+        }
+    }
+
+    /**
+     * Reaction to a pointer down event while mapping is active
+     *
+     * @param event - Touch Event
+     */
+    private void onMappingPointerDown(MotionEvent event){
+        float sourceX = event.getX(0);
+        float sourceY = event.getY(0);
+        float xOfInterest = event.getX(1);
+        float yOfInterest = event.getY(1);
+        applyDijkstra(currentImage, (int)xOfInterest, (int)yOfInterest, (int)sourceX, (int)sourceY);
+        maping = false;
     }
 }
--- app/src/main/java/com/example/scott/dalmapproject/MapActivity.java
+++ app/src/main/java/com/example/scott/dalmapproject/MapActivity.java
@@ -13,102 +13,185 @@
 package com.example.scott.dalmapproject;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapActivity extends AppCompatActivity{
+public class MapActivity extends AppCompatActivity implements GoogleMap.OnMyLocationButtonClickListener,
+        OnMapReadyCallback {
 
-    private static final String TAG = "MapActivity";
-    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
-    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
-    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
-
-    private boolean mLocationPermissionGranted = false;
     private GoogleMap mMap;
+    public static final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 99;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        getLocationPermission();
+        //check if the location permission has already been granted
+        checkLocationPermission();
 
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
     }
 
     /**
-     * Initializes the map. sets mMap.
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
      */
-    private void initMap(){
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
 
-        mapFragment.getMapAsync(new OnMapReadyCallback() {
+        // Show zoom buttons
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        // Remove all labels from the map
+        mMap.setMapStyle(new MapStyleOptions(getResources()
+                .getString(R.string.style_json)));
+
+        // Check for location permission
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_CONTACTS},
+                        MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+            }
+        } else {
+            // Permission has already been granted
+            mMap.setMyLocationEnabled(true);
+        }
+
+        // Show the My Location button
+        mMap.setOnMyLocationButtonClickListener(this);
+
+        // Add a marker at the killam
+        LatLng killam = new LatLng(44.637391666666666, -63.591011111111115);
+        mMap.addMarker(new MarkerOptions().position(killam).title("Killam Memorial Library"));
+
+        // Add a marker at the goldberg
+        LatLng goldberg = new LatLng(44.63741111111111, -63.58735277777778);
+        mMap.addMarker(new MarkerOptions().position(goldberg).title("Goldberg Computer Science Building"));
+
+        // Add a marker at the henry hicks
+        LatLng henryHicks = new LatLng(44.636175, -63.59314166666667);
+        mMap.addMarker(new MarkerOptions().position(henryHicks).title("Henry Hicks Building"));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(killam));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(killam,15.5f));
+
+    }
+
+    /**
+     * Functionality for the killam floor button
+     */
+    private void killamFloorPlanButtonFunctionality(){
+        Button killamFloorPlanButton = (Button) findViewById(R.id.exterior_to_killam_button);
+        killamFloorPlanButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onMapReady(GoogleMap googleMap) {
-                Toast.makeText(MapActivity.this, "Map is ready", Toast.LENGTH_SHORT).show();
-                Log.d(TAG, "onMapReady: initializing map");
-                mMap = googleMap;
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), KillamFloorPlanActivity.class);
+                startActivity(intent);
             }
         });
     }
 
     /**
-     * gets the location permissions for the map.
+     * Functionality for when the location button is clicked
+     *
+     * @return - false so event callback animates the camera to the users location
      */
-    private void getLocationPermission(){
-        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
-
-        Log.d(TAG, "getLocationPermission: getting location permissions");
-        
-        if(ContextCompat.checkSelfPermission(this.getApplicationContext(), FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-            if(ContextCompat.checkSelfPermission(this.getApplicationContext(), COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-                //SET A BOOLEAN
-                mLocationPermissionGranted = true;
-            }
-            else{
-                ActivityCompat.requestPermissions(this,permissions,LOCATION_PERMISSION_REQUEST_CODE);
-            }
-        }
+    @Override
+    public boolean onMyLocationButtonClick() {
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
     }
 
     /**
-     * gets the permissions results.
-     * @param requestCode code from request.
-     * @param permissions permissions being requested.
-     * @param grantResults the results if permissions were granted or not.
+     * checks for and requests location permission
+     *
+     * @return - returns based on whether of not the permission was requested
      */
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        mLocationPermissionGranted = false;
+    public boolean checkLocationPermission() {
 
-        Log.d(TAG, "onRequestPermissionsResult: called");
-        
-        switch(requestCode){
-            case LOCATION_PERMISSION_REQUEST_CODE:{
-                if(grantResults.length > 0){
-                    for(int i = 0; i < grantResults.length; i++){
-                        if(grantResults[i] != PackageManager.PERMISSION_GRANTED){
-                            mLocationPermissionGranted = false;
-                            Log.d(TAG, "onRequestPermissionsResult: permission failed");
-                            return;
-                        }
-                    }
-                    Log.d(TAG, "onRequestPermissionsResult: permission granted");
-                    mLocationPermissionGranted = true;
-                    //initialize the map
-                    initMap();
-                }
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                //Prompt the user once explanation has been shown
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+    /**
+     * Checks if the permission was granted or denied
+     *
+     * @param requestCode - Specific code for the permission being requested
+     * @param permissions - list of permissions being requested
+     * @param grantResults - results of the permission request
+     */
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+
+        if (requestCode == MY_PERMISSIONS_REQUEST_FINE_LOCATION) {
+            if (permissions.length == 1 &&
+                    permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // permission granted
+                mMap.setMyLocationEnabled(true);
+            } else {
+                // permission denied
             }
         }
     }
